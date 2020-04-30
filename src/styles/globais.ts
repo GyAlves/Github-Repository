e9b180b08a95a7daa91b05e30117b6df5fbbcd77
Gyasmin <gyasminassuncao@uni9.edu.br>
2020-04-29 21:19:52 -0300

    Primeiro projeto em react

---
+++ src/styles/globais.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    outline:0;
+    box-sizing:border-box
+  }
+
+  body{
+    background:#F0F0F5;
+    -webkit-font-smoothing:antialiased
+  }
+  body,input,button{
+    font:16px sans-serif;
+  }
+
+  #root{
+    max-width:960px;
+    margin:0;
+    padding:40px, 20px;
+  }
+  button{
+    cursor:pointer;
+  }
+`;

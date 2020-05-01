d4cd5661cff9308bdbce7058e34f56739ff755da
Gyasmin <gyasminassuncao@uni9.edu.br>
2020-05-01 13:03:49 -0300

    Estilizando o Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore diferentes repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="github logo" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img src="" alt="avatar" />
+          <div>
+            <strong>Gyas/projects</strong>
+            <p>My personal projects developed during the gostack bootcamp</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
